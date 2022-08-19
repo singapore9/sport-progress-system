7a4b1393fd5fd50c74ba135526906d3199c49e11
singapore9 <sharik.and@gmail.com>
2022-08-19 19:11:57 +0300

    Additional charts with avg, max, total count

--- templates/chartBuilder.js
+++ templates/chartBuilder.js
@@ -1,10 +1,182 @@
+function onlyDate(datetime) {
+    datetime.setMilliseconds(0);
+    datetime.setSeconds(0);
+    datetime.setMinutes(0);
+    datetime.setHours(0);
+}
+
+function tabataChart(sinceDate, workoutItemsData, toggleFunction) {
+    let workouts = [];
+    let exerciseName = "tabata";
+    let color = "#369EAD";
+
+    $.each(workoutItemsData, function(key, val) {
+        if (val['name'] !== exerciseName) {
+            return;
+        }
+        workouts.push([
+            val["iterations_count"],
+            val["timestamp"]
+        ]);
+    });
+
+    let specifiedExerciseData = [];
+    workouts.forEach(function(workoutItem, workoutInd) {
+        let itemDate = new Date(1000 * workoutItem[1]);
+        if (itemDate > sinceDate) {
+            onlyDate(itemDate);
+            specifiedExerciseData.push({
+                x: itemDate,
+                y: 1
+            });
+        }
+    });
+    specifiedExerciseData.sort((a, b) => a['x'] - b['x']);
+
+    dataConfigs = [{
+        type: "column",
+        name: exerciseName,
+        color: color,
+        showInLegend: true,
+        axisYIndex: -1,
+        dataPoints: specifiedExerciseData
+    }];
+
+    let axisYConfigs = [{
+        includeZero: true
+    }];
+    let chart = new CanvasJS.Chart("tabataChartContainer", {
+        title: {
+            text: "Tabata by Day"
+        },
+        axisY: axisYConfigs,
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleFunction
+        },
+        data: dataConfigs
+    });
+    chart.render();
+}
+
+function calculateAvgMaxSum(sinceDate, workoutItemsData, exerciseName) {
+    let workoutsByDate = {};
+    let filteredWorkoutItems = [];
+    let dates = [];
+    let calculationsByDate = {};
+
+    $.each(workoutItemsData, function(key, val) {
+        if (val['name'] !== exerciseName) {
+            return;
+        }
+        workoutItemDate = new Date(1000 * val['timestamp']);
+        onlyDate(workoutItemDate);
+        let workoutTimestamp = String(workoutItemDate.getTime());
+
+        if (workoutsByDate.hasOwnProperty(workoutTimestamp)) {
+            workoutsByDate[workoutTimestamp].push(
+                val["iterations_count"]
+            );
+        } else {
+            workoutsByDate[workoutTimestamp] = [
+                val["iterations_count"]
+            ];
+            dates.push(workoutTimestamp);
+        }
+    });
+
+    dates.forEach(function(item, ind) {
+        let iterationsArray = workoutsByDate[item];
+        let iterationsSum = iterationsArray.reduce((partialSum, a) => partialSum + a, 0);
+        let iterationsMax = Math.max(...iterationsArray);
+        let iterationsAvg = iterationsSum / iterationsArray.length;
+        calculationsByDate[item] = [iterationsAvg, iterationsMax, iterationsSum];
+    });
+    return calculationsByDate;
+}
+
+function makeExerciseDetailedChart(exerciseName, exerciseDisplayName, chartId, sinceDate, workoutItemsData, toggleFunction) {
+    let calculationsByDate = calculateAvgMaxSum(sinceDate, workoutItemsData, exerciseName);
+    let colors = ["#369EAD", "#C24642", "#7F6084"];
+    let prefixes = ["Average ", "Max ", "Total "];
+
+    let axisYConfigs = [];
+    let dataConfigs = [];
+    let maxY = 0;
+
+    colors.forEach(function(item, ind) {
+        let title = prefixes[ind] + exerciseDisplayName;
+
+        let specifiedExerciseData = [];
+        for (const property in calculationsByDate) {
+            let date = new Date(Number(property))
+            specifiedExerciseData.push({
+                'x': date,
+                'y': calculationsByDate[property][ind]
+            });
+            if (calculationsByDate[property][ind] > maxY) {
+                maxY = calculationsByDate[property][ind];
+            }
+        }
+
+        specifiedExerciseData.sort((a, b) => a['x'] - b['x']);
+        dataConfigs.push({
+            type: "line",
+            name: title,
+            color: item,
+            showInLegend: true,
+            axisYIndex: ind - 1,
+            dataPoints: specifiedExerciseData
+        });
+    });
+
+    axisYConfigs = [{
+        includeZero: true,
+        maximum: maxY * 1.1
+    }];
+    dataConfigs[1].visible = false;
+
+    let chart = new CanvasJS.Chart(chartId, {
+        title: {
+            text: "Detailed " + exerciseDisplayName
+        },
+        axisY: axisYConfigs,
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleFunction
+        },
+        data: dataConfigs
+    });
+    chart.render();
+}
+
+function pullupsChart(sinceDate, workoutItemsData, toggleFunction) {
+    let exerciseName = "pull-ups";
+    let exerciseDisplayName = "Pull Ups";
+    let chartId = "pullupsChartContainer";
+    makeExerciseDetailedChart(exerciseName, exerciseDisplayName, chartId, sinceDate, workoutItemsData, toggleFunction);
+}
+
+
+function legsraisesChart(sinceDate, workoutItemsData, toggleFunction) {
+    let exerciseName = "legs_raises";
+    let exerciseDisplayName = "Legs Raises";
+    let chartId = "legsraisesChartContainer";
+    makeExerciseDetailedChart(exerciseName, exerciseDisplayName, chartId, sinceDate, workoutItemsData, toggleFunction);
+
+}
+
 window.onload = function() {
-    var rawWorkoutItems = [];
-    var workoutsByName = {};
-    var workoutsNames = [];
-    var monthAgo = new Date();
+    let monthAgo = new Date();
     monthAgo.setMonth(monthAgo.getMonth() - 1);
-    var userFilter = location.search.slice(1);
+    let userFilter = location.search.slice(1);
+    let filteredWorkoutItems = [];
 
     $.getJSON("/api/workout-item", function(data) {
         $.each(data, function(key, val) {
@@ -12,81 +184,12 @@ window.onload = function() {
                 if (valItem['user_id'] !== userFilter) {
                     return;
                 }
-                console.log(valItem);
-                rawWorkoutItems.push(valItem);
-                var workoutName = valItem["name"];
-                console.log(workoutName);
-                if (workoutsByName.hasOwnProperty(workoutName)) {
-                    workoutsByName[workoutName].push([
-                        valItem["iterations_count"],
-                        valItem["timestamp"]
-                    ]);
-                    console.log("add iterations_count");
-                } else {
-                    workoutsByName[workoutName] = [
-                        [valItem["iterations_count"], valItem["timestamp"]]
-                    ];
-                    workoutsNames.push(workoutName);
-                    console.log("add workout name + iterations_count");
-                }
-            });
+                filteredWorkoutItems.push(valItem);
+            })
         });
-
-        var axisYConfigs = [];
-        var dataConfigs = [];
-        var colors = ["#369EAD", "#C24642", "#7F6084"];
-        workoutsNames.forEach(function(item, ind) {
-            var newColor = colors[ind % 3];
-
-            axisYConfigs.push({
-                title: item,
-                lineColor: newColor,
-                tickColor: newColor,
-                labelFontColor: newColor,
-                titleFontColor: newColor,
-                includeZero: true
-            });
-
-            var specifiedExerciseData = [];
-            workoutsByName[item].forEach(function(workoutItem, workoutInd) {
-                var itemDate = new Date(1000 * workoutItem[1]);
-                if (itemDate > monthAgo) {
-                    specifiedExerciseData.push({
-                        x: itemDate,
-                        y: workoutItem[0]
-                    });
-                }
-            });
-            specifiedExerciseData.sort((a, b) => a['x'] - b['x']);
-            dataConfigs.push({
-                type: "line",
-                name: item,
-                color: newColor,
-                showInLegend: true,
-                axisYIndex: ind - 1,
-                dataPoints: specifiedExerciseData
-            });
-        });
-
-        console.log(workoutsByName);
-        console.log(workoutsNames);
-        console.log(dataConfigs);
-
-        var chart = new CanvasJS.Chart("chartContainer", {
-            title: {
-                text: "Workouts By Exercise Type"
-            },
-            axisY: axisYConfigs,
-            toolTip: {
-                shared: true
-            },
-            legend: {
-                cursor: "pointer",
-                itemclick: toggleDataSeries
-            },
-            data: dataConfigs
-        });
-        chart.render();
+        tabataChart(monthAgo, filteredWorkoutItems, toggleDataSeries);
+        pullupsChart(monthAgo, filteredWorkoutItems, toggleDataSeries);
+        legsraisesChart(monthAgo, filteredWorkoutItems, toggleDataSeries);
     });
 
     function toggleDataSeries(e) {
