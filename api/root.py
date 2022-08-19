2c44011769420e35e33121f1da6e1106f01acdd7
singapore9 <sharik.and@gmail.com>
2022-08-19 07:49:27 +0300

    Add ability to get languages for specific users

--- api/root.py
+++ api/root.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from api.exercise import router as exercise_router
 from api.workout_item import router as workout_item_router
+from api.language import router as language_router
 
 
 router = APIRouter(prefix="/api")
 router.include_router(exercise_router)
 router.include_router(workout_item_router)
+router.include_router(language_router)
--- firebase.py
+++ firebase.py
@@ -12,6 +12,8 @@ EXERCISES_NEXT_ID_PATH = f"/{EXERCISES_NEXT_ID_KEY}/"
 EXERCISES_PATH = f"/{EXERCISES_KEY}/"
 WORKOUT_ITEMS_NEXT_ID_PATH = f"/{WORKOUT_ITEMS_NEXT_ID_KEY}/"
 WORKOUT_ITEMS_PATH = f"/{WORKOUT_ITEMS_KEY}/"
+LANGUAGE_ITEMS_PATH = "/language/users/"
+
 
 cred_obj = firebase_admin.credentials.Certificate(FIREBASE_CERTIFICATE)
 default_app = firebase_admin.initialize_app(cred_obj, {
